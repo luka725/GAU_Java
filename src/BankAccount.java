5c2b50748dbae6efc13755fa207b0f4caad79c88
luka <luka.sheylashvili@gmail.com>
2024-03-18 14:33:56 +0400

    adding one task

---
+++ src/BankAccount.java
@@ -0,0 +1,25 @@
+public class BankAccount {
+    public String OwnerName;
+    public String OwnerLastname;
+    public String AccountId;
+    private float Money;
+    public BankAccount(String name, String last_name, String id){
+        this.OwnerName = name;
+        this.OwnerLastname = last_name;
+        this.AccountId = id;
+    }
+    public void AddMoney(float amount){
+        Money += amount;
+    }
+    public void WithdrawMoney(float amount){
+        if(Money - amount < 0){
+            System.out.println("Not enough money in the account!");
+        }
+        else{
+            Money -= amount;
+        }
+    }
+    public float GetMoney(){
+        return Money;
+    }
+}
